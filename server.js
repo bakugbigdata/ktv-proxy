405e7e5cf9ae3eaac46625d205275341a29a1760
bakugbigdata <bakugbigdata@gmail.com>
2025-12-17 19:42:53 +0900

    add health check endpoint

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import "dotenv/config";
 
 const app = express();
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("KTV proxy alive");
+});
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
